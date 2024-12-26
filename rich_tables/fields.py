01f940b4ceb96ec9ea40d4153ac4e63e7cef0316
Šarūnas Nejus <snejus@protonmail.com>
2024-12-26 00:00:29 +0000

    Automatic backup 2024-12-25

--- rich_tables/fields.py
+++ rich_tables/fields.py
@@ -256,7 +256,7 @@ fields_by_func: dict[Callable[..., RenderableType], Iterable[str]] = {
         "mastering",
         "media",
         "module",
-        "name",
+        # "name",
         "operation",
         "primary",
         "priority",
